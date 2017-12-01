8a155129da82f23bfeaed903e20aa3d7258df4cc
ap080221063 <ap080221063@gmail.com>
2017-12-01 11:26:49 +0000

    typescript transformation

---
+++ logic.js
@@ -0,0 +1,9 @@
+var Square = (function () {
+    function Square(top, bottom, left, right) {
+        this.top = top;
+        this.bottom = bottom;
+        this.left = left;
+        this.right = right;
+    }
+    return Square;
+})();
